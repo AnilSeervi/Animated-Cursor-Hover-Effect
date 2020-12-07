8969b031d0e0be948efd1a268f1c3190ed831587
Anil Seervi <anil13112000@gmail.com>
2020-12-07 19:09:10 +0530

    Initial Setup.

---
+++ app.js
@@ -0,0 +1,22 @@
+(function () {
+  const link = document.querySelectorAll("nav > .hover-this");
+  const cursor = document.querySelector(".cursor");
+  const animate = (e) => {
+    const span = document.querySelector("span");
+    const { offSetX: x, offSetY: y } = e,
+      { offsetWidth: width, offsetHeight: height } = this,
+      move = 25,
+      xMove = (x / width) * (move * 2) - move,
+      yMove = (y / height) * (move * 2) - move;
+    span.style.transform = `translate(${xMove}px,${yMove}px)`;
+    if (e.type === "mouseleave") span.style.transform = ``;
+  };
+  const editCursor = (e) => {
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+  };
+  link.forEach((b) => b.addEventListener("mousemove", animate));
+  link.forEach((b) => b.addEventListener("mouseleave", animate));
+  window.addEventListener("mousemove", editCursor);
+})();
